57926117c6ee19ced1ccf6cf491a8e7599d673a3
Kristine Kalinina <kviljeva@gmail.com>
2026-02-25 21:40:02 +0100

    first task

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 import http from "http";
-const port = 3333;
+import fs from "fs";
+
+
 const server = http.createServer((req, res) => {
+
+  const log = new Date() + " | " + req.method + " | " + req.url + "\n";
+
+  fs.appendFile("requests.log", log, (err) => {
+    if (err) {
+      console.log("Error writing to file");
+    }
+  });
+
+  
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  console.log(req.headers);
-  if (req.method === "GET") {
-    if (req.url === "/") {
-      res.end("Main Page");
-    } else if (req.url === "/about") {
-      res.end("About Page");
-    } else if (req.url === "/contacts") {
-      res.end("Contacts Page");
-    } else {
-      res.statusCode = 404;
-      res.end("Page not found");
-    }
-  } else if (req.method === "POST") {
-    if (req.url === "/submit") {
-      req.end("Form submitted!");
-    } else {
-      res.statusCode = 404;
-      res.end("Page not found");
-    }
-  } else {
-    res.statusCode = 405;
-    res.end("Method not allowed");
-  }
+  res.end("Request logged");
+
 });
-server.listen(port, () => {
-  console.log(`Server is running at http://127.0.0.1:${port}`);
+
+
+server.listen(3000, () => {
+  console.log("Server running at http://127.0.0.1:3000");
 });
